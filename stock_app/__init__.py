24bf5053527557a9463272e74333c4d2576a25bc
Abdullah <markosberg123@gmail.com>
2021-02-16 01:20:48 +0200

    fix Procfile5

--- stock_app/__init__.py
+++ stock_app/__init__.py
@@ -6,13 +6,13 @@ from stock_app.config import configurations
 
 def create_app(environment_name='dev'):
     app = Flask(__name__)
-
     app.config.from_object(configurations[environment_name])
+
+    app.register_blueprint(home)
+    app.register_blueprint(stock, url_prefix='/stock')
 
     @app.errorhandler(500)
     def handle_error(exception):
         return render_template('500.html'), 500
 
-    app.register_blueprint(home)
-    app.register_blueprint(stock, url_prefix='/stock')
     return app
